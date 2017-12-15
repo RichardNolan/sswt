a5b98db9d22a193e6bc8e43cd825b5d9ba3fe385
Richard Nolan <therichardnolan@gmail.com>
2017-12-15 12:48:26 +0000

    resave merge conflict file

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-    // $('.navbar .dropdown').hover(function() {
-    //     $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideDown();
-    // }, function() {
-    //     $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-    // });
+
 
 console.log("READY")
     $('a').bind('ajax:success', function(event, data, status, xhr) {
@@ -19,20 +14,9 @@ console.log("READY")
         $like.html("<img src='/images/heart_red.svg' class='heart' />  <span class='product_likes'>"+ product_likes +"</span>")
       });
 
-    $('.dropdown-toggle').on('click', function(e){
-        
-        console.log("CLICK")
-=======
-    $('.navbar .dropdown').hover(function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideDown();
-    }, function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
-    });
-
 
 
     $(document).on('click', '.sidekick_image', function(){
         $('#hero_image').attr('src', $(this).attr('src'))
->>>>>>> images
     })
 })
