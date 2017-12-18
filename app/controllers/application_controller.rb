b3d59596e7be85acaf82159359e4ffd7a2fff049
rayphelan <rayphelan@gmail.com>
2017-12-18 15:50:42 +0000

    Admin can ban producer from entire website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,18 @@ class ApplicationController < ActionController::Base
   #before_action :county_list
   
 
+  # Check if Producer is disabled, except for logging out
+  before_action :producer_enabled, :except => [:destroy]
+
+
   # Check if producer is disabled by admin
   def producer_enabled
-    if producer_signed_in? && current_producer.enabled == false
-      redirect_to producer_not_allowed_path
+    redirected = false
+    if producer_signed_in? && current_producer.enabled == false && redirected == false
+      redirected = true
+      render 'producers/not_allowed'
     end
+    false
   end
 
 
