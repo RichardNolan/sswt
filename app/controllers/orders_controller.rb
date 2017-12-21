e625947860b66e78b64cb8e7cee2d03f8cef6e99
rayphelan <rayphelan@gmail.com>
2017-12-21 14:08:52 +0000

    Removed unneccessary orders files

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,9 +5,7 @@ class OrdersController < ApplicationController
 
   # Set @orders Variable
   before_action :set_order, only: [:show, :edit, :update, :destroy, :order_items]
-  
-  # before_action :authenticate_admin!, only: [:index]
-  
+    
 
   # Get Order Items - Ajax Request
   def order_items
@@ -17,8 +15,7 @@ class OrdersController < ApplicationController
   end
 
 
-  # GET /orders
-  # GET /orders.json
+  # Orders page
   def index
     if(customer_signed_in?) then
       @orders = Order.where('customer_id = ?', current_customer.id).order(updated_at: :desc)
@@ -30,24 +27,8 @@ class OrdersController < ApplicationController
       @total = @order.reduce(0) {|total, item| total + (item['q']*item['p'])}
       render 'session'
     end
-    #@orders = Order.all
   end
 
-
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-  end
-
-  # GET /orders/new
-  def new
-    @order = Order.new
-  end
-
-  # GET /orders/1/edit
-  def edit
-  end
 
   # A generic catch all redirect if there is an error with the unregistered order
   def custom_error
@@ -55,6 +36,8 @@ class OrdersController < ApplicationController
     # return false
   end
 
+
+  # Verify order details page
   def verify
     if(customer_signed_in?) then
       # @order = Hamper.where('customer_id = ?', current_customer.id)
@@ -153,29 +136,6 @@ class OrdersController < ApplicationController
 
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /orders/1
-  # DELETE /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
